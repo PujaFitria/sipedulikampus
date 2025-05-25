fccb9a2c7c00606e898222b6f50440d8f8cb82ae
PujaFitria <148304418+PujaFitria@users.noreply.github.com>
2025-05-25 17:40:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,12 +7,9 @@ if(isset($_GET['menu'])){
         include "masuk.php";
     }else if($_GET['menu']==6){
         include "akun.php";
-    }else if($_GET['menu']==7){
-        include "sop.php";
     }else {
         echo "Pilih Menu";
     }
 }else{   
 }
-include "footer.php";
 ?>
