0ffbf29073507a39cd36363431c14c205898c581
PujaFitria <148304418+PujaFitria@users.noreply.github.com>
2025-05-26 09:46:38 +0700

    Update akun.php

--- akun.php
+++ akun.php
@@ -1,37 +1,34 @@
-<div class="col-lg-8">
-    <div class="card mt-5">
-        <div class="card-body">
-            <h5 class="card-title">Kelola Akun</h5>
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>Email</th>
-                        <th>Password</th>
-                        <th>action</th>
-                    </tr>
-                </thead>
-                <?php
-                   include 'koneksi.php';
-                   $data = $conn->query("select * from tbl_masuk");
-                ?>
-                <tbody>
-                    <?php
-                      $no =1;
-                      while($row=$data->fetch_assoc()){
-                    ?>
-                    <tr>
-                        <td><?php echo $no; ?></td>
-                        <td><?php echo $row['email']; ?></td>
-                        <td><?php echo $row['pass']; ?></td>
-                        <td><a class="badge bg-warning text-wrap" href="hapus.php?id_hapus=<?php echo $row['id']?>">hapus</a></td>
-                    </tr>
-                    <?php
-                      $no++;
-                      }
-                    ?>
-                </tbody>
-            </table>   
-        </div>
+<div class="mt-5">
+  <?php
+    include 'koneksi.php';
+
+    // Cek koneksi terlebih dahulu
+    if ($conn->connect_error) {
+        die("Koneksi gagal: " . $conn->connect_error);
+    }
+
+    $data = $conn->query("SELECT * FROM tbl_user");
+
+    // Cek apakah query berhasil dijalankan
+    if ($data) {
+      while ($row = $data->fetch_assoc()) {
+  ?>
+  <div class="card mt-5" style="width: 18rem;">
+    <h5 class="fs-4 fw-bold text-center mt-2" style="color: #104c40">Profil</h5>
+    <img src="img/prita.jpg" width="100" height="100" class="rounded-circle mx-auto mt-1" alt="prita">
+    <div class="card-body text-center">
+      <label class="btn-sm btn-success">Edit Foto<input type="file" name="file" hidden></label>
     </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item"><?php echo $row['nama']; ?></li>
+      <li class="list-group-item"><?php echo $row['role']; ?></li>
+      <li class="list-group-item"><?php echo $row['email']; ?></li>
+    </ul>
+  </div>
+  <?php
+      } // tutup while
+    } else {
+        echo "Gagal menampilkan data: " . $conn->error;
+    }
+  ?>
 </div>
