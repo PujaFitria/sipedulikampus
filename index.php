97e38a373999d597f9494e322b63357adf9e9d98
PujaFitria <148304418+PujaFitria@users.noreply.github.com>
2025-05-26 09:56:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,1 @@
-<?php 
-//Isi atau Content 
-//http://localhost/sipedulikampus/index.html
 
-if(isset($_GET['menu'])){
-    if($_GET['menu']==5){
-        include "masuk.php";
-    }else if($_GET['menu']==6){
-        include "akun.php";
-    }else {
-        echo "Pilih Menu";
-    }
-}else{   
-}
-?>
